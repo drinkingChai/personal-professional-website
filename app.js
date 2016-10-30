2c2d116ab0c43207815b27e50fe9e4ce98307015
Wasif Zaman <teatocode@gmail.com>
2016-10-30 02:04:52 -0400

    Add route and project factory

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var app = require('./server/app');
+var app = express();
 
 app.use(express.static('app/public'));
+
+var projects = require('./routes/projects');
+app.use('/projects', projects);
 
 module.exports = app;
---
+++ routes/projects.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+redisClient.select(('profWebsiteTest' || 'development').length);
+
+var router = express.Router();
+
+router.route('/')
+  .get(function(request, response) {
+    redisClient.hget('projects', function(error, data) {
+      response.status(200).json(data);
+    });
+  })
+  .post(parseUrlencoded, function(request, response) {
+    var newProject = request.body;
+    redisClient.hset('projects', newProject.name, newProject.description, function(error) {
+      if (error) throw error;
+      response.status(201).json(newProject.name);
+    })
+  });
+
+router.route('/:name')
+  .get(function(request, response) {
+    redisClient.hget('projects', request.params.name, function(error, data) {
+      response.status(200).json(data);
+    });
+  })
+  .delete(function(request, response) {
+    redisClient.hdel('projects', request.params.name, function(error) {
+      if (error) throw error;
+      response.sendStatus(204);
+    });
+  });
+
+module.exports = router;
