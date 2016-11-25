3309400c1dde8e475f53ad0925171beb74ea75b9
Wasif Zaman <teatocode@gmail.com>
2016-11-24 22:15:43 -0500

    Add, remove tags to projects and filter by tags

--- public/assets/js/directives/pwTagModule.js
+++ public/assets/js/directives/pwTagModule.js
@@ -10,22 +10,38 @@ angular.module('ProfessionalWebsite')
       controller: function($scope) {
         $scope.tag = new Tag();
         $scope.tags = Tag.query();
-        $scope.createTag = function(tag) {
-          Tag.query().$promise.then(function(data) {
-            if (!$scope.project.hasOwnProperty("tags")) { $scope.project.tags = {}; }
-            for (var i = 0, l = data.length; i < l; i++) {
-              if (tag.name === data[i].name) {
-                $scope.project.tags[tag.name] = true;
-                $scope.tag = new Tag();
-                return;
-              }
-            }
-            tag.$save();
-            $scope.project.tags[tag.name] = true;
-            $scope.tags = Tag.query();
-            $scope.tag = new Tag();
-          });
+
+        $scope.init = function() {
+          $scope.temp = new Set($scope.project.tags || []);
         }
+
+        $scope.toggleTag = function(tag) {
+          if (!$scope.temp.has(tag)) {
+            $scope.temp.add(tag);
+          } else {
+            $scope.temp.delete(tag);
+          }
+
+          $scope.project.tags = Array.from($scope.temp).sort();
+        }
+
+
+        // $scope.createTag = function(tag) {
+        //   Tag.query().$promise.then(function(data) {
+        //     if (!$scope.project.hasOwnProperty("tags")) { $scope.project.tags = {}; }
+        //     for (var i = 0, l = data.length; i < l; i++) {
+        //       if (tag.name === data[i].name) {
+        //         $scope.project.tags[tag.name] = true;
+        //         $scope.tag = new Tag();
+        //         return;
+        //       }
+        //     }
+        //     tag.$save();
+        //     $scope.project.tags[tag.name] = true;
+        //     $scope.tags = Tag.query();
+        //     $scope.tag = new Tag();
+        //   });
+        // }
       }
     }
   });
