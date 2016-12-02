f82bca9a1f764410537a9dc4a66826713e7e92d7
Wasif Zaman <teatocode@gmail.com>
2016-12-02 01:18:36 -0500

    Remove background from code

--- public/assets/js/directives/pwPageFooter.js
+++ public/assets/js/directives/pwPageFooter.js
@@ -6,22 +6,3 @@ angular.module('ProfessionalWebsite')
       templateUrl: "assets/templates/directives/pwPageFooter.html"
     }
   });
-
-
-function(){
-  var toCode = function(mockup) {
-    return function(chai) {
-      return mockup * chai;
-    };
-  }
-
-  project1 = toCode(mockup_1);
-  product_v1 = project1(bengal_chai);
-  product_v2 = project1(masala_chai);
-  product_v3 = project1(spiced_masala_chai);
-
-  emailToClient({
-    "Subject": "Spiced Masala Chai yielded best result!!",
-    "Body": " ... "
-  });
-}
